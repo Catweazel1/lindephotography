7b42a44c8e3ce08f9d907a0a16ba4054b73c4f87
Lonnie Dean Linde <lonnie_linde@hotmail.com>
2018-04-18 23:05:17 +0200

    Updated files to work with bootstrap

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 	<title>Linde Photography - Welcome</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--	<script type="text/javascript" src="js/jquery.js"></script>
+<	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/popper.js"></script>
 	<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"> -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
