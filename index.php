d341fb07ba9dc0a191847b1a18056222b2a6d39b
Lonnie Dean Linde <lonnie_linde@hotmail.com>
2018-04-18 22:56:15 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Linde Photography - Welcome</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/popper.js"></script>
+	<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"> -->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+</body>
+</html>
