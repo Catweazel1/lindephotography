cd8d6f892b055210ffae7640e7082d866b7c9ac0
Lonnie Dean Linde <lonnie_linde@hotmail.com>
2018-04-19 22:34:24 +0200

    Add Logo and Title to page

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
 	<title>Linde Photography - Welcome</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/popper.js"></script>
 	<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-
+	<div id="container">
+		<h1 class="title"><img src="images/logo.png"> Linde Photography</h1>
+		<h4 class="title">Naturally Beautiful Pictures</h4>
+	</div>
 </body>
 </html>
