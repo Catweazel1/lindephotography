d8ef59118e71c7885ba6adaa084965f3130937e0
TacocaTNL <garslade359@gmail.com>
2018-07-07 23:00:29 +0200

    Changed to login with strong encryption

--- admin/login.php
+++ admin/login.php
@@ -16,11 +16,11 @@
 </head>
 <body>
 	<div class="container">
-		<form method="post" action="login.php">
+		<form method="post" action="login.php" class="form">
 			<h1>Administration Login Form</h1>
-			<div>User Name: </div><div><input type="text" name="user_name"></div>
-			<div>Password: </div><div><input type="password" name="user_pass"></div>
-			<div><input type="submit" value="Login" name="login"></div>
+			<div>User Name: </div><div><input type="text" name="username" class="form-control"></div>
+			<div>Password: </div><div><input type="password" name="password" class="form-control"></div>
+			<div><input type="submit" value="Login" name="login" class="btn btn-info"></div>
 		</form>
 	</div>
 </body>
@@ -29,21 +29,34 @@
 <?php 
 	include('includes/connect.php');
 	if(isset($_GET['login'])) {
-		$user_name = mysqli_escape_string($connect, $_POST['user_name']);
-		$user_pass = mysqli_escape_string($connect, $_POST['user_pass']);
-		$salt = '$5$rounds=5050$ihaventmadeupmymindyet$';
+		$username = mysqli_real_escape_string($connect, $_POST['username']);
+		$password = mysqli_real_escape_string($connect, $_POST['password']);
+		
 
-		$encrypt = crypt($user_pass, $salt);
-
-		$admin_query = "SELECT * FROM users WHERE username = '$user_name' and password = '$encrypt'";
+		$admin_query = "SELECT * FROM users WHERE username = '$username'";
 
 		$result = mysqli_query($connect, $admin_query);
-		$count = mysqli_num_rows($result);
+		
+		if(mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_array($result)) {
+				if(password_varify($password, $row['password'])) {
+					// return true;
+					$_SESSION['username'] = $username;
+					header("location: index.php");
+				} else {
+					// return false;
+					echo "<script>alert('Password is incorrect.')</script>"
+				}
+			}
+		} else {
+			echo "<script>alert('Username and / or Password are incorrect.')</script>";
+		}
+		/*
 		if($count == 1) {
-			$_SESSION['username'] = $user_name;
+			$_SESSION['username'] = $username;
 			header("location: index.php");
 		} else {
 			echo "<script>alert('Username or password is incorrect')</script>";
-		}
+		}*/
 	}
 ?>
