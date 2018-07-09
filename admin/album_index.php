e73c56eee20b01d1215318dc04cc3b6da3af1647
TacocaTNL <garslade359@gmail.com>
2018-07-09 20:23:47 +0200

    Moved standard block of text from each file to include file in html_header.php

--- admin/album_index.php
+++ admin/album_index.php
@@ -9,15 +9,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/admin.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/popper.js"></script>
-	<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+	<?php include('includes/html_header.php'); ?>
 	<title>Foto Album</title>
-	<link rel="stylesheet" type="text/css" href="css/admin.css">
 </head>
 <body>
 	<div class="container">
--- admin/login.php
+++ admin/login.php
@@ -6,13 +6,7 @@
 <html>
 <head>
 	<title>Admin Login</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/admin.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/popper.js"></script>
-	<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+	<?php include('includes/html_header.php'); ?>
 </head>
 <body>
 	<div class="container">
