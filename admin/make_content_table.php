6afe65d835bf61869159b0768fa5d1858ff023b5
TacocaTNL <garslade359@gmail.com>
2018-07-09 21:13:47 +0200

    Added Make_Content_Table to site

---
+++ admin/make_content_table.php
@@ -0,0 +1,11 @@
+<?php 
+	$query = "select id, contents from contents"
+	$result = mysqli_query($connect, $query);
+	echo "<div class='row'>";
+	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+		$content_id = $row['id'];
+		$contents = $row['contents'];
+		echo "<div class='col-sm'>$contents</div><div class='col-sm'><a href='edit_album.php?id=$content_id'>Edit</a></div><div class='col-sm'><a href='delete_album.php?id=$content_id'>Delete</a></div>";
+	}
+	echo "</div>";
+?>
