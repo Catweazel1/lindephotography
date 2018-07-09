473bbb56042e3c8fd4feeb9853b5ffe09ce54292
Lonnie Dean Linde <lonnie_linde@hotmail.com>
2018-07-09 22:24:43 +0200

    Added ability to add albums to site.

---
+++ admin/new_album.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>New Album</title>
+	<?php 
+		include('includes/html_header.php');
+	?>
+</head>
+<body>
+	<form class="form" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+		New Album: <input type="text" name="album" class="form-control">
+		<input type="submit" name="submit" value="Submit" class="btn btn-info">
+	</form>
+</body>
+</html>
+<?php 
+	if(isset($_POST['submit']) && !empty($_POST['album'])) {
+		include('includes/connect.php');
+		$album = $_POST['album'];
+		$query = "insert into contents(contents) values ($album)";
+		mysqli_query($connect, $query);
+		if(!file_exists('../images/'.$album)) {
+			mkdir('../images/' . $album, 0777, true);
+		}
+		header("location: album_index.php");
+	} else {
+		echo "<script>alert('Please fill in album name!');</script>";
+	}
+?>
