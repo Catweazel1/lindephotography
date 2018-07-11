4ca9ab77b276b588fadf1c0659c0ffe8816a0569
TacocaTNL <garslade359@gmail.com>
2018-07-11 21:19:00 +0200

    Added possibility to delete Albums where pictures are still in folder.

---
+++ admin/delete_album.php
@@ -0,0 +1,21 @@
+<?php 
+	include('includes/html_header.php');
+	include('includes/connect.php');
+	include('includes/functions.php');
+
+	if(isset($_GET['id'])) {
+		$id = $_GET['id'];
+		$query = "DELETE FROM pictures WHERE content_id = '$id'";
+		mysqli_query($connect, $query);
+
+		$query = "SELECT contents from contents WHERE id = '$id'";
+		$result = mysqli_query($connect, $query);
+		$row = mysqli_fetch_array($result, MYSQLI_NUM);
+		$contents = $row[0];
+
+		$query = "DELETE FROM contents WHERE id = '$id'";
+		mysqli_query($connect, $query);
+		deleteAll('../images/') . $contents;
+		header("location: album_index.php");
+	}
+?>
