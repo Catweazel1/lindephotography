51736c96c88748c4fafca47418f96561a13d5a7e
TacocaTNL <garslade359@gmail.com>
2018-07-13 22:06:10 +0200

    Added thumbnails to page.

---
+++ admin/make_album_table.php
@@ -0,0 +1,22 @@
+<div class="row">
+		<div class="col-sm">Title</div><div class="col-sm">Photo</div><div class="col-sm">Date Added</div><div class="col-sm">Delete?</div>
+</div>
+
+<?php 
+	$query = "SELECT contents FROM contents WHERE id = '$id';"
+	$result = mysqli_query($connect, $query);
+	while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+		$photo_id = $row['id'];
+		$title = $row['title'];
+		$photo = $row['picture'];
+		$photo = $contents[0] . "/" . $photo;
+		$date = $row['post_date'];
+
+		echo "<div class='row'>
+			<div class='col-sm'>$title</div>
+			<a class='col-sm' href='edit_photo.php?id=$photo_id'><img src='../images/$photo' width='150px'></a>
+			<div class='col-sm'>$date</div>
+			<a class='col-sm' href='delete_photo.php?id=$photo_id'>Yes</a>
+		</div>";
+	}
+?>
