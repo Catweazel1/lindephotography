95b2d34793e89e157638b92aabd6c1759b728f7c
TacocaTNL <garslade359@gmail.com>
2018-07-13 23:05:29 +0200

    Added ability to rename photo albums

---
+++ admin/rename_album.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rename Album</title>
+	<?php 
+		include('includes/html_header.php');
+	?>
+</head>
+<body>
+	<?php 
+		if(isset($_GET['id'])) {
+			global $id;
+			$id = $_GET['id'];
+			include('includes/connect.php');
+			$query = "SELECT contents FROM contents WHERE id = '$id'";
+			$result = mysqli_query($connect, $query);
+			while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+				if(empty($row)) {
+					$album = 'empty';
+				} else {
+					$album = $row[0];
+				}
+			}
+		}
+
+		if(isset($_POST['submit'])) {
+			$album = $_POST['album'];
+			$album = mysqli_real_escape_string($connect, $album);
+			$contents_query = "SELECT contents FROM contents WHERE id = '$id'";
+			$contents = mysqli_query($connect, $contents_query);
+			$content_result = mysqli_fetch_row($contents);
+			$query = "UPDATE contents SET contents='$album' WHERE id = '$id'";
+			mysqli_query($connect, $query) or die(myqli_error($connect));
+			rename("../images/" . htmlspecialchars($content_result[0]), "../images/" . htmlspecialchars_decode($album));
+			header("location: album_index.php");
+		}
+	?>
+
+	<form class="form" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" >
+		Rename Album: <input class="form-control" type="text" name="album" value="<?php echo $album; ?>" >
+		<input class="btn btn-info" type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
