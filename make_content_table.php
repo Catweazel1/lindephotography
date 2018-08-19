b0ba91df1626a6351da5fd213702cfdd286b56c7
TacocaTNL <garslade359@gmail.com>
2018-08-19 16:09:22 +0200

    Added a "table" structure to contents

--- make_content_table.php
+++ make_content_table.php
@@ -3,9 +3,17 @@
 
 	$query = 'SELECT id, contents from contents';
 	$result = mysqli_query($connect, $query);
+	$i = 0;
+	echo "<div class='row'>";
 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+		if(($i > 0) && ($i % 3 === 0)) {
+			echo "</div>
+			<div class='row'>";
+		};
+		$i++;
 		$content_id = $row['id'];
 		$contents = $row['contents'];
-		echo "<a href='cat_idx.php?id=$content_id;'>$contents</a>";
+		echo "<a class='col-sm' href='cat_idx.php?id=$content_id;'>$contents</a>";
 	}
+	echo "</div>";
 ?>
